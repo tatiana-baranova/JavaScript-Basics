14d37db70e07ee6ab23d80389e2f06d6dccca490
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-06-25 20:59:09 +0300

    add operators

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,27 @@
 var myFirstVar;
 var mySecond = "Hello";
 
-console.log("Result: " + mySecond);
+// console.log("Result: " + mySecond);
 
 mySecond = 5; 
 
-console.log("Result: " + mySecond);
+// console.log("Result: " + mySecond);
 
 var some = "some";
 some = 5.4;
-console.log(some);
+// console.log(some);
 
 
 let num = 50;
 num = 45;
-console.log(num);
+// console.log(num);
 
 
 const Num = 5;
 // Num = 6;
-console.log(Num);
+// console.log(Num);
+
+
 
 
 
---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+let x = 5;
+let y = 10;
+let res; 
+
+res = x + y;
+console.log("Result:"+ res);
+
+res = x - y;
+console.log("Result:"+ res);
+
+res = x * y;
+console.log("Result:"+ res);
+
+res = x / y;
+console.log("Result:"+ res);
+
+res += 1;
+console.log("Result:"+ res);
+
+let a = 5;
+res = (a + y) * 2;
+console.log("Result:"+ res);
+
+
+let w1 = "Hello ";
+let w2 = "World";
+console.log(w1 + w2);
