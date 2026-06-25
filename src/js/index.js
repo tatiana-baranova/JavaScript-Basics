8f8e6744802a31378bfabecb4f2d3ee8fed8f5b2
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-06-25 20:44:18 +0300

    Variables and data types

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
-console.log("Hello World!");
+var myFirstVar;
+var mySecond = "Hello";
 
-console.error('Error');
+console.log("Result: " + mySecond);
+
+mySecond = 5; 
+
+console.log("Result: " + mySecond);
+
+var some = "some";
+some = 5.4;
+console.log(some);
+
+
+let num = 50;
+num = 45;
+console.log(num);
+
+
+const Num = 5;
+// Num = 6;
+console.log(Num);
+
+
+
