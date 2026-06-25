2bb655a6c9d898a9d3b5b12f630f039c2b204512
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-06-25 21:45:11 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,41 @@
-let x = 5;
-let y = 10;
-let res; 
+// let x = 5;
+// let y = 10;
+// let res; 
 
-res = x + y;
-console.log("Result:"+ res);
+// res = x + y;
+// console.log("Result:"+ res);
 
-res = x - y;
-console.log("Result:"+ res);
+// res = x - y;
+// console.log("Result:"+ res);
 
-res = x * y;
-console.log("Result:"+ res);
+// res = x * y;
+// console.log("Result:"+ res);
 
-res = x / y;
-console.log("Result:"+ res);
+// res = x / y;
+// console.log("Result:"+ res);
 
-res += 1;
-console.log("Result:"+ res);
+// res += 1;
+// console.log("Result:"+ res);
 
-let a = 5;
-res = (a + y) * 2;
-console.log("Result:"+ res);
+// let a = 5;
+// res = (a + y) * 2;
+// console.log("Result:"+ res);
 
 
-let w1 = "Hello ";
-let w2 = "World";
-console.log(w1 + w2);
+// let w1 = "Hello ";
+// let w2 = "World";
+// console.log(w1 + w2);
+
+let x = 16;
+let hasCar = false;
+let y = 16;
+if (y == x || hasCar == true )
+console.log("Рівність");
+else if(x >= 15){
+    console.warn("x >= 15");
+    if(x = 16)
+        console.log("x == 16");
+}
+else
+    console.log("Not equal");
+    
